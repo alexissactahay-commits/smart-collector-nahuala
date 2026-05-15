d2c378420fc2095e953bb9925965ea14d441788b
Jairo Sac <jsactahay@gmail.com>
2026-05-14 19:25:27 -0600

    Correccion visual boton regresar pequeno en Users

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -62,9 +62,9 @@ const Users = () => {
   // =========================
   <div className="users-back-row">
   <button
-    id="btn-regresar-users"
     type="button"
-    onClick={handleBack}
+    className="btn-back-users"
+    onClick={() => navigate('/admin-dashboard')}
   >
     ← Regresar
   </button>
